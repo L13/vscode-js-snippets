a0de805bd4ed7d69b584b698079903f69067207b
L13 <L13.RARY@gmail.com>
2023-03-25 21:19:34 +0100

    - added cache and real time settings - fixed languages for providers

--- src/extension.ts
+++ src/extension.ts
@@ -5,9 +5,11 @@ import * as vscode from 'vscode';
 import javascriptFuncs from './snippets/javascript.funcs';
 import typescriptFuncs from './snippets/typescript.funcs';
 
+import type { Snippet } from './types';
+
 //	Variables __________________________________________________________________
 
-
+let useFunctionBlockPadding = true;
 
 //	Initialize _________________________________________________________________
 
@@ -17,33 +19,12 @@ import typescriptFuncs from './snippets/typescript.funcs';
 
 export function activate (context: vscode.ExtensionContext) {
 	
-	const useFunctionBlockPadding = get('useFunctionBlockPadding');
-
-	function buildSnippet (item: { prefix: string, body: string[] }) {
-		
-		const snippet = new vscode.CompletionItem(item.prefix);
-		const body = useFunctionBlockPadding ? item.body : filterEmptyTab(item.body);
-		
-		snippet.insertText = new vscode.SnippetString(body.join('\n'));
-		
-		return snippet;
-		
-	}
+	useFunctionBlockPadding = get('useFunctionBlockPadding');
+	
+	let cacheJavaScriptSnippets = buildSnippets(javascriptFuncs);
+	let cacheTypeScriptSnippets = buildSnippets(typescriptFuncs);
 	
 	const javascriptProvider = vscode.languages.registerCompletionItemProvider([
-		'javascript',
-		'javascriptreact',
-	], {
-		
-		provideCompletionItems () {
-			
-			return Object.values(javascriptFuncs).map(buildSnippet);
-			
-		},
-		
-	});
-	
-	const typescriptProvider = vscode.languages.registerCompletionItemProvider([
 		'javascript',
 		'javascriptreact',
 		'typescript',
@@ -52,13 +33,36 @@ export function activate (context: vscode.ExtensionContext) {
 		
 		provideCompletionItems () {
 			
-			return Object.values(typescriptFuncs).map(buildSnippet);
+			return cacheJavaScriptSnippets;
+			
+		},
+		
+	});
+	
+	const typescriptProvider = vscode.languages.registerCompletionItemProvider([
+		'typescript',
+		'typescriptreact',
+	], {
+		
+		provideCompletionItems () {
+			
+			return cacheTypeScriptSnippets;
 			
 		},
 		
 	});
 	
 	context.subscriptions.push(javascriptProvider, typescriptProvider);
+	
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration((event) => {
+		
+		if (event.affectsConfiguration('l13JSSnippets.useFunctionBlockPadding')) {
+			useFunctionBlockPadding = get('useFunctionBlockPadding');
+			cacheJavaScriptSnippets = buildSnippets(javascriptFuncs);
+			cacheTypeScriptSnippets = buildSnippets(typescriptFuncs);
+		}
+		
+	}));
 	
 }
 
@@ -73,5 +77,22 @@ function get (key: string, value?: any) {
 function filterEmptyTab (body: string[]) {
 	
 	return body.filter((value) => value !== '\t');
+	
+}
+
+function buildSnippet (item: Snippet) {
+	
+	const body = useFunctionBlockPadding ? item.body : filterEmptyTab(item.body);
+	const snippet = new vscode.CompletionItem(item.prefix, vscode.CompletionItemKind.Snippet);
+	
+	snippet.insertText = new vscode.SnippetString(body.join('\n'));
+	
+	return snippet;
+	
+}
+
+function buildSnippets (snippets: Record<string, Snippet>) {
+	
+	return Object.values(snippets).map(buildSnippet);
 	
 }
